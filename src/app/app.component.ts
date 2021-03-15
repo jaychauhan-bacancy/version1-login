786313d7c6823ad24a1bad86a6ca515434df913f
jaychauhan-bacancy <jay.chauhan@bacancy.com>
2021-03-15 09:36:27 +0530

    login-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+// import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { NgForm } from '@angular/forms'; 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'version1';
+  joinroom:boolean=false;
+  createroom:boolean=false;
+  // @ViewChild('f') signupForm: NgForm;
+  myimage:string="assets/image.gif";
+  onEdit()
+  {
+    this.joinroom=true;
+  }
+  onPress()
+  {
+    this.createroom=true;
+  }
 }
